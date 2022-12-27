1a6b46bddc9fdfa845fcef46c9aadbf213b910f4
SanazGh <sanaz.ghafouri1@gmail.com>
2022-12-27 21:48:33 +0330

    add final app

---
+++ src/app.js
@@ -0,0 +1,141 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+  }
+
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[day];
+  }
+  
+  function displayForecast(response) {
+    let forecast = response.data.daily;
+  
+    let forecastElement = document.querySelector("#forecast");
+  
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+      if (index < 6) {
+        forecastHTML =
+          forecastHTML +
+          `
+          <div class="card col-5">
+          <div class="title text-center mt-2">
+          <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+            <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+              forecastDay.condition.icon_url}
+            alt=""
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temperature.maximum
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temperature.minimum
+          )}° </span>
+            </div>
+          </div>
+        </div>
+    `;
+      }
+    });
+  
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+
+  function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "4372515633ef2c6aat609051o83db044";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=38.71667&lon=-9.13333&key=${apiKey}`;
+    axios.get(apiUrl).then(displayForecast);
+  }
+  
+  function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+  
+    celsiusTemp = response.data.temperature.current;
+
+  
+    temperatureElement.innerHTML = Math.round(celsiusTemp);
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.time * 1000);
+    iconElement.setAttribute(
+      "src",
+      "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+    );
+    iconElement.setAttribute("alt", response.data.condition.icon_url);
+
+  getForecast(response.data.coordinates);
+  }
+  
+  function search(city) {
+    let apiKey = "4372515633ef2c6aat609051o83db044";
+    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+  
+  function pressSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+  }
+  
+  function showFahrenheitTemp(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    let fahrenheiTemp = (celsiusTemp * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheiTemp);
+  }
+  
+  function showCelsiusTemp(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemp);
+  }
+  
+  let celsiusTemp = null;
+  
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", pressSubmit);
+  
+  let fahrenheitLink = document.querySelector("#fahrenheit-degree");
+  fahrenheitLink.addEventListener("click", showFahrenheitTemp);
+  
+  let celsiusLink = document.querySelector("#celsius-degree");
+  celsiusLink.addEventListener("click", showCelsiusTemp);
+  
+  search("Lisbon");
